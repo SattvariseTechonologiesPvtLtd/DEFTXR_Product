26e6b550b91461fd1a8fe0448d6a86574ffb52c7
Yash Wayase <201700106@vupune.ac.in>
2020-11-21 16:01:57 +0530

    Dev 101:Update On Fibia And Patella Bones.
    Tasks Done By Yash Wayase:
    
    1) Added Tibia Bone and Patella Bone To the Cloud Build.
    2) Updated the Game Manager Script of Both the bones.
    3) Completed With the ligament,insertion,origin part of the tibia bone & ligament,insertion part of the patella bone.
    4) Added to the path :
     \Assets\DEFTXR\Human Anatomy\Skeletal System\ Tibia.
    &
     \Assets\DEFTXR\Human Anatomy\Skeletal System\ Patella.

---
+++ DEFTXR_VR_Cloud/Assets/Patella_GameManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Patella_GameManager : MonoBehaviour
+{
+    // public GameObject ZoneA, ZoneB, ZoneC;
+    public GameObject zoneBObj;
+
+    public GameObject insertionObj,  DefaultObj, ligamentObj;
+
+    public bool attch, inserAttch, ligamentAttach, origAttach = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onInsertionButtonClick()
+    {
+        if (inserAttch == false)
+        {
+
+            insertionObj.SetActive(true);
+     
+            DefaultObj.SetActive(false);
+            ligamentObj.SetActive(false);
+
+
+
+            inserAttch = true;
+        }
+        else
+        {
+
+            insertionObj.SetActive(false);
+          
+            DefaultObj.SetActive(true);
+            ligamentObj.SetActive(false);
+
+            inserAttch = false;
+        }
+    }
+
+   
+
+    public void onLigamentsButtonClick()
+    {
+        if (ligamentAttach == false)
+        {
+
+            insertionObj.SetActive(false);
+          
+            DefaultObj.SetActive(false);
+            ligamentObj.SetActive(true);
+            ligamentAttach = true;
+        }
+        else
+        {
+
+
+            insertionObj.SetActive(false);
+          
+            DefaultObj.SetActive(true);
+            ligamentObj.SetActive(false);
+            ligamentAttach = false;
+        }
+    }
+
+
+}
