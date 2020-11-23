27e671822434fce5942a4f071e108e089c9c51ca
Yash Wayase <201700106@vupune.ac.in>
2020-11-23 14:10:27 +0530

    Dev 103: Update On HipBone
    Tasks Done By Yash:-
    1) Added HipBone To The Cloud Build.
    2) Updated the GameManager Script of the bone.
    Tasks Done By Jhanvi :-
    3) Completed with the Ligament,insertion,Origin  Part of the bone.
    4)  Added to the path :
    \Assets\DEFTXR\Human Anatomy\Skeletal System\ HipBone.

---
+++ DEFTXR_VR_Cloud/Assets/DEFTXR/Human Anatomy/Skeletal System/Hip Bone/Scripts_Yash/HipBone_GameManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HipBone_GameManager : MonoBehaviour
+{
+    // public GameObject ZoneA, ZoneB, ZoneC;
+    public GameObject zoneBObj;
+    public GameObject HipBoneinsertionObj, HipBoneoriginObj, HipBoneclavDefaultObj, HipBoneligamentObj;
+    //public GameObject pecuBtn, sidedeterBtn, featureBtn, clinAnaBtn, ossiBtn;
+    public GameObject HipBoneligamentBtn, HipBoneinsertioBtn, HipBoneoriginBtn;
+    public Sprite selected, Dselected;
+    public bool attch, HipBoneinserAttch, HipBoneligamentAttach, HipBoneorigAttach = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClickandSelect();
+    }
+
+    void ClickandSelect()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+
+
+
+            if (string.Compare(UIManager.Instance.selectedBtnName, "Insertion") == 0)
+            {
+                if (HipBoneinserAttch == false)
+                {
+                    HipBoneinsertioBtn.GetComponent<Image>().sprite = selected;
+                    HipBoneinsertionObj.SetActive(true);
+                    HipBoneoriginObj.SetActive(false);
+                    HipBoneclavDefaultObj.SetActive(false);
+                    HipBoneligamentObj.SetActive(false);
+
+
+
+                    HipBoneinserAttch = true;
+                }
+                else
+                {
+                    HipBoneinsertioBtn.GetComponent<Image>().sprite = Dselected;
+                    HipBoneinsertionObj.SetActive(false);
+                    HipBoneoriginObj.SetActive(false);
+                    HipBoneclavDefaultObj.SetActive(true);
+                    HipBoneligamentObj.SetActive(false);
+
+                    HipBoneinserAttch = false;
+                }
+            }
+
+            if (string.Compare(UIManager.Instance.selectedBtnName, "Origin") == 0)
+            {
+                if (HipBoneorigAttach == false)
+                {
+                    HipBoneoriginBtn.GetComponent<Image>().sprite = selected;
+                    HipBoneinsertionObj.SetActive(false);
+                    HipBoneoriginObj.SetActive(true);
+                    HipBoneclavDefaultObj.SetActive(false);
+                    HipBoneligamentObj.SetActive(false);
+                    HipBoneorigAttach = true;
+                }
+                else
+                {
+
+                    HipBoneoriginBtn.GetComponent<Image>().sprite = Dselected;
+                    HipBoneinsertionObj.SetActive(false);
+                    HipBoneoriginObj.SetActive(false);
+                    HipBoneclavDefaultObj.SetActive(true);
+                    HipBoneligamentObj.SetActive(false);
+                    HipBoneorigAttach = false;
+                }
+            }
+
+            if (string.Compare(UIManager.Instance.selectedBtnName, "Ligaments") == 0)
+            {
+
+                if (HipBoneligamentAttach == false)
+                {
+                    HipBoneligamentBtn.GetComponent<Image>().sprite = selected;
+                    HipBoneinsertionObj.SetActive(false);
+                    HipBoneoriginObj.SetActive(false);
+                    HipBoneclavDefaultObj.SetActive(false);
+                    HipBoneligamentObj.SetActive(true);
+                    HipBoneligamentAttach = true;
+                }
+                else
+                {
+
+                    HipBoneligamentBtn.GetComponent<Image>().sprite = Dselected;
+                    HipBoneinsertionObj.SetActive(false);
+                    HipBoneoriginObj.SetActive(false);
+                    HipBoneclavDefaultObj.SetActive(true);
+                    HipBoneligamentObj.SetActive(false);
+                    HipBoneligamentAttach = false;
+                }
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
