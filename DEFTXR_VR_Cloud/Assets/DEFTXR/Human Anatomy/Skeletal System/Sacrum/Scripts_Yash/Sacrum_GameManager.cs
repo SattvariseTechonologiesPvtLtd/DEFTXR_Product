88d1f327c1ed3316c752a3d2b842160ce224496a
Yash Wayase <201700106@vupune.ac.in>
2020-12-02 13:15:45 +0530

    Dev 107: Update On Sacrum Bone
    Tasks Done By Yash Wayase:-
    1) Added Sacrum Bone To the Cloud Build.
    2) Updated the Game Manager Script of the Sacrum bone.
    3) Completed With the ligament,,origin part of the Sacrum bone.
    4) Added to the path :
    \Assets\DEFTXR\Human Anatomy\Skeletal System\ Sacrum.

---
+++ DEFTXR_VR_Cloud/Assets/DEFTXR/Human Anatomy/Skeletal System/Sacrum/Scripts_Yash/Sacrum_GameManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Sacrum_GameManager : MonoBehaviour
+{
+    public GameObject zoneBObj;
+    public GameObject SacrumoriginObj, SacrumDefaultObj, SacrumligamentObj;
+
+    public bool attch, ligamentAttach, origAttach = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+    public void onOriginButtonClick()
+    {
+        if (origAttach == false)
+        {
+
+            
+            SacrumoriginObj.SetActive(true);
+            SacrumDefaultObj.SetActive(false);
+            SacrumligamentObj.SetActive(false);
+            origAttach = true;
+        }
+        else
+        {
+
+
+          
+            SacrumoriginObj.SetActive(false);
+            SacrumDefaultObj.SetActive(true);
+            SacrumligamentObj.SetActive(false);
+            origAttach = false;
+        }
+    }
+
+    public void onLigamentsButtonClick()
+    {
+        if (ligamentAttach == false)
+        {
+
+       
+            SacrumoriginObj.SetActive(false);
+            SacrumDefaultObj.SetActive(false);
+            SacrumligamentObj.SetActive(true);
+            ligamentAttach = true;
+        }
+        else
+        {
+
+
+           
+            SacrumoriginObj.SetActive(false);
+            SacrumDefaultObj.SetActive(true);
+            SacrumligamentObj.SetActive(false);
+            ligamentAttach = false;
+        }
+    }
+}
