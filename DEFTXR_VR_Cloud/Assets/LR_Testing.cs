93c7c5c45eb59653b4a1a48ca55aca51efa60c22
Sujit Mane <manesujit85@gmail.com>
2021-04-28 11:35:47 +0530

    Sujit Mane : New Line Renderer
    Sujit Mane : New Line Renderer

---
+++ DEFTXR_VR_Cloud/Assets/LR_Testing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LR_Testing : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] points;
+    [SerializeField]
+    private LineController line;
+
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+}
